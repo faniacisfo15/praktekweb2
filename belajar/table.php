d1c5b0734ef20547ce1fb1ca104d93d08592b7f3
faniacisfo98 <fania.sisfountan@gmail.com>
2017-05-03 13:55:01 +0700

    belajar require dan include 3 mei 2017

---
+++ belajar/table.php
@@ -0,0 +1,14 @@
+	<table border="1">
+			<tr>
+				<td>No</td> 
+				<td>Nim</td>
+				<td>Nama</td>
+			</tr>
+			
+			<tr>
+				<td>1</td>
+				<td>H1101151024</td>
+				<td>Fania</td>
+			</tr>
+			
+	</table>
